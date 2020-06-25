fd8d3bc3ca95a2fc42a41e7ea6fdf1c3d969c43b
Krishna Kompalli <kompalligk@gmail.com>
2020-06-26 00:00:31 +0530

    done

---
+++ user-posts-backend-nodeJS-mongoDB/app.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const postsRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(
+    "mongodb+srv://kris:Dystopiancity1@cluster0-lz5gv.mongodb.net/users-posts?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join("images")));
+
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.use("/api/posts", postsRoutes);
+app.use("/api/user", userRoutes);
+
+app.listen(port, ()=>{
+  console.log("port working @"+port)
+})
+
+module.exports = app;
